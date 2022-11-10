9098583381859c28b5249e8b56639c05d3ad6d5c
Marcus <hello@marcusskov.com>
2022-11-10 23:21:33 +0000

    Cleanup of phantom_camera_base.gd

--- addons/phantom_camera/phantom_camera_base.gd
+++ addons/phantom_camera/phantom_camera_base.gd
@@ -4,53 +4,22 @@ extends Node3D
 var _active_camera: Node3D
 var _active_phan_cam_list: Array[int]
 
-@onready var _camera:= get_parent()
-
-#func _enter_tree() -> void:
-#	print("The parent is: ", get_parent())
+var _camera
 
 func _enter_tree() -> void:
-	if _camera is Camera3D:
-		print("Camera is Camera3D")
-	elif _camera is Camera2D:
-		print("Camera is Camera2D")
-	else:
-		print("Is not a child of a Camera")
+	_camera = get_parent()
+#	if _camera is Camera3D:
+#		print("Camera is Camera3D")
+#	elif _camera is Camera2D:
+#		print("Camera is Camera2D")
+#	else:
+#		printerr("Is not a child of a Camera")
 
 	add_to_group(PhantomCameraManager.PHANTOM_CAMERA_BASE_GROUP_NAME)
-	PhantomCameraManager.update_check_pcam_base_group()
 
 
 func _exit_tree() -> void:
 	remove_from_group(PhantomCameraManager.PHANTOM_CAMERA_BASE_GROUP_NAME)
-	PhantomCameraManager.update_check_pcam_base_group()
-#func set_active_cam(phan_cam: Node3D) -> void:
-##	print(_active_phan_cam_list)
-##	_active_phan_cam_list.
-#	var phan_cam_id: int = phan_cam.get_instance_id()
-#
-#	if not _active_phan_cam_list.has(phan_cam_id):
-#		_active_phan_cam_list.append(phan_cam_id)
-#	else:
-#		_active_phan_cam_list.pop_at(_active_phan_cam_list.find(phan_cam_id))
-#		_active_phan_cam_list.append(phan_cam_id)
-#
-#	_active_camera = phan_cam
-##	print(phan_cam.name)
-#
-##	print("Current cam list after adding of cam is:", _active_phan_cam_list)
-#
-#
-#func remove_phan_cam_from_list(phan_cam: Node3D) -> void:
-#
-#	var phan_cam_id: int = phan_cam.get_instance_id()
-#
-#	if _active_camera == phan_cam:
-#		_active_phan_cam_list.pop_at(_active_phan_cam_list.find(phan_cam_id))
-#	else:
-#		_active_phan_cam_list.pop_at(_active_phan_cam_list.find(phan_cam_id))
-#
-##	print("Current cam list after removal of cam is:", _active_phan_cam_list)
 
 
 #func _process(delta: float) -> void:
