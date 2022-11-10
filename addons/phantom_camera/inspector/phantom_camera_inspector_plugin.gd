825bb1edd3173f82b21977b96b01597cb1369a4b
Marcus <hello@marcusskov.com>
2022-11-10 23:20:50 +0000

    Disabled custom inspector plugin until work resumes

--- addons/phantom_camera/inspector/phantom_camera_inspector_plugin.gd
+++ addons/phantom_camera/inspector/phantom_camera_inspector_plugin.gd
@@ -3,27 +3,44 @@ extends EditorInspectorPlugin
 
 var _phantom_camera_script: Script = preload("res://addons/phantom_camera/phantom_camera.gd")
 
-func _can_handle(object) -> bool:
-	return object is _phantom_camera_script
+
+# 	TODO - Enable again once work is resumed for inspector based tasks
+
+#func _can_handle(object) -> bool:
+#	return object is _phantom_camera_script
 
 
 func _parse_category(object: Object, category: String) -> void:
+
 	var _margin_container: MarginContainer = MarginContainer.new()
-#	_margin_container.add_theme_constant_override("margin_left", 10)
-#	_margin_container.add_theme_constant_override("margin_top", 50)
-#	_margin_container.add_theme_constant_override("margin_right", 10)
-#	_margin_container.add_theme_constant_override("margin_bottom", 50)
+	var _margin_v: float = 20
+	_margin_container.add_theme_constant_override("margin_left", 10)
+	_margin_container.add_theme_constant_override("margin_top", _margin_v)
+	_margin_container.add_theme_constant_override("margin_right", 10)
+	_margin_container.add_theme_constant_override("margin_bottom", _margin_v)
+	add_custom_control(_margin_container)
+
+	var _vbox_container: VBoxContainer = VBoxContainer.new()
+	_margin_container.add_child(_vbox_container)
 
 	var align_with_view_button = Button.new()
 	align_with_view_button.connect("pressed", _align_camera_with_view.bind(object))
 	align_with_view_button.set_custom_minimum_size(Vector2(0, 60))
 	align_with_view_button.set_text("Align with view")
-#	print("Object is: ", object)
-#	print("Category is: ", category)
-	_margin_container.add_child(align_with_view_button)
-	add_custom_control(_margin_container)
+	_vbox_container.add_child(align_with_view_button)
+
+	var preview_camera_button = Button.new()
+	preview_camera_button.connect("pressed", _preview_camera.bind(object))
+	preview_camera_button.set_custom_minimum_size(Vector2(0, 60))
+	preview_camera_button.set_text("Preview Camera")
+	_vbox_container.add_child(preview_camera_button)
+
 
 
 func _align_camera_with_view(object: Object) -> void:
 	print("Aligning camera with view")
+	print(object)
+
+func _preview_camera(object: Object) -> void:
+	print("Previewing camera")
 	print(object)
