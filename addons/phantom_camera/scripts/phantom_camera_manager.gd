3b2dea78a2c904925535bf1f7cc830361915eff1
Marcus <hello@marcusskov.com>
2022-11-28 21:06:33 +0000

    Added look_at target transition to tween

--- addons/phantom_camera/scripts/phantom_camera_manager.gd
+++ addons/phantom_camera/scripts/phantom_camera_manager.gd
@@ -49,7 +49,6 @@ func phantom_camera_removed_from_scene(pcam) -> void:
 
 func find_pcam_with_highest_priority() -> void:
 	for pcam_item in pcam_list:
-#		TODO - Should also check whether if the existing active cam
 		if pcam_item.priority > _active_pcam_priority:
 			_active_pcam = pcam_item
 			_active_pcam_priority = pcam_item.priority
@@ -58,7 +57,7 @@ func find_pcam_with_highest_priority() -> void:
 
 
 func pcam_priority_updated(pcam) -> void:
-	if pcam.priority > _active_pcam_priority and pcam != _active_pcam:
+	if pcam.priority >= _active_pcam_priority and pcam != _active_pcam:
 		_active_pcam = pcam
 		_active_pcam_priority = pcam.priority
 		_pcam_tween_complete = false
@@ -68,7 +67,7 @@ func pcam_priority_updated(pcam) -> void:
 
 
 	elif pcam == _active_pcam:
-		if pcam.priority < _active_pcam_priority:
+		if pcam.priority <= _active_pcam_priority:
 			_active_pcam_priority = pcam.priority
 			_pcam_tween_complete = false
 			find_pcam_with_highest_priority()
@@ -85,38 +84,29 @@ func _pcam_tween_transition(duration: float, pcam_changed_mid_tween: bool = fals
 		_pcam_tween_property(duration)
 		_pcam_tween.tween_callback(pcam_tween_complete)
 	else:
-#		print("Target move during tween")
 		_pcam_tween_elasped_time_duration -= _pcam_tween.get_total_elapsed_time()
 		_pcam_tween.kill()
 		_pcam_tween = get_tree().create_tween()
 		_pcam_tween_property(_pcam_tween_elasped_time_duration)
 		_pcam_tween.tween_callback(pcam_tween_complete)
 
+
 func _pcam_tween_property(duration: float) -> void:
 	if _active_pcam._follow_target_offset:
 		_pcam_tween.parallel().tween_property(camera_base_3D, "position", _active_pcam.get_position(), duration)
 
 	if _active_pcam.has_look_at_target:
-		print("Starting look_at tween")
-		_pcam_tween.parallel().tween_method(camera_base_3D.look_at.bind(Vector3.UP), Vector3(0,0,0), Vector3(0,0,0), duration)
+		var _new_pcam_position: Vector3 = _active_pcam.get_position()
+		_pcam_tween.parallel().tween_property(camera_base_3D, "rotation", _active_pcam.get_rotation(), duration)
 
 
 func pcam_tween_complete() -> void:
-	print("Tween complete")
 	_active_pcam_has_changed = false
-
 	_pcam_tween_complete = true
-
 	_pcam_position_changed = false
 
 
 func _process(delta: float) -> void:
-#	if camera_base_2D:
-#		print("Has camera 2D")
-#		TODO - Assign 2D camera to _active_pcam
-
-#	_active_pcam_has_changed = false
-#	if camera_base_3D && not _active_pcam_has_changed:
 
 	if camera_base_3D:
 		if _pcam_tween_complete:
