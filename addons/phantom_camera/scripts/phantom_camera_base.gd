3a12b994b7c5f2988c7c20379c03e9ee0186f76e
Marcus <hello@marcusskov.com>
2022-11-28 21:06:57 +0000

    Minor Camera Base cleanup

--- addons/phantom_camera/scripts/phantom_camera_base.gd
+++ addons/phantom_camera/scripts/phantom_camera_base.gd
@@ -1,9 +1,6 @@
 @tool
 extends Node
 
-var _active_camera: Node3D
-var _active_phan_cam_list: Array[int]
-
 var _camera
 
 func _enter_tree() -> void:
@@ -26,15 +23,3 @@ func _exit_tree() -> void:
 	elif PhantomCameraManager.camera_base_3D == _camera:
 		PhantomCameraManager.camera_base_3D = null
 
-
-#func _process(delta: float) -> void:
-#	if (_active_phan_cam_list.size() > 0 && _camera):
-#		pass
-#		print(main_camera)
-#		_camera.set_position(_active_camera.position)
-#		_camera.set_rotation(_active_camera.get_rotation())
-
-#		print(_active_camera.position)
-#		print(_active_camera.name)
-#		camera.position = _active_camera.position
-#		print(_active_camera.is_camera_active)
