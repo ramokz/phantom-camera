38529a4cb35acc2612a4f6f1edb6e0b09a70c0ee
Marcus <hello@marcusskov.com>
2022-12-11 22:44:19 +0000

    Updated phantom_camera_3D.gd with variables and constants from phantom_camera_variables and phantom_camera_constants respectively

--- addons/phantom_camera/scripts/phantom_camera_3D.gd
+++ addons/phantom_camera/scripts/phantom_camera_3D.gd
@@ -3,91 +3,37 @@ class_name PhantomCamera3D
 extends Node3D
 @icon("res://addons/phantom_camera/icons/PhantomCameraIcon3D.svg")
 
-#####################
-# General - Variables
-#####################
-var _priority_property_name: StringName = "Priority"
-var _priority: int = 1
-const PHANTOM_CAMERA_GROUP_NAME: StringName = "phantom_camera_group"
-const PHANTOM_CAMERA_BASE_GROUP_NAME: StringName = "phantom_camera_base_group"
+const PhantomCameraConstants = preload("phantom_camera/phantom_camera_constants.gd")
+var Phantom_Camera_Variables = preload("phantom_camera/phantom_camera_variables.gd").new()
 
-##############
-# Phantom base
-##############
-var camera_base_group: Array
-var phantom_camera_base_owner: PhantomCameraHost3D
-var scene_has_multiple_phantom_camera_bases: bool
+var phantom_camera_host_owner: PhantomCameraHost3D
 
-##################
-# Follow - Variables
-##################
-var _follow_target_property_name: StringName = "Follow Target"
-var follow_target_node: Node
-var _follow_target_path: NodePath
-var has_follow_target: bool = false
-
-var _follow_target_offset_property_name: StringName = "Follow Parameters/Follow Target Offset"
 var follow_target_offset: Vector3 = Vector3(0, 0, 3)
 
 ###################
 # Look At - Variables
 ###################
-var _look_at_target_property_name: StringName = "Look At Target"
+const _look_at_target_property_name: StringName = "Look At Target"
 var look_at_target_node: Node
 var _look_at_target_path: NodePath
 var has_look_at_target: bool = false
 
-var _look_at_target_offset_property_name: StringName = "Look At Parameters/Look At Target Offset"
+const _look_at_target_offset_property_name: StringName = "Look At Parameters/Look At Target Offset"
 var look_at_target_offset: Vector3
-
-#################
-# Tween - Variables
-#################
-var _tween_transition_property_name: StringName = "Tween Properties / Transition"
-var tween_transition: Tween.TransitionType
 var tween_linear: bool
-enum TweenTransitions {
-	TRANS_LINEAR = 0,
-	TRANS_SINE = 1,
-	TRANS_QUINT = 2,
-	TRANS_QUART = 3,
-	TRANS_QUAD = 4,
-	TRANS_EXPO = 5,
-	TRANS_ELASTIC = 6,
-	TRANS_CUBIC = 7,
-	TRANS_CIRC = 8,
-	TRANS_BOUNCE = 9,
-	TRANS_BACK = 10,
-	CUSTOM = 11
-}
 
-
-var _tween_ease_property_name: StringName = "Tween Properties / Ease"
-var tween_ease: Tween.EaseType
-enum TweenEases {
-	EASE_IN = 0,
-	EASE_OUT = 1,
-	EASE_IN_OUT = 2,
-	EASE_OUT_IN = 3,
-}
-
-var _tween_duration_property_name: StringName = "Tween Properties / Duration"
-var tween_duration: float = 1
-
-#	TODO - Camera Smoothing
-#@export_range(0, 100, 1, "or_greater") var camera_smoothing: float = 0
 
 ############
 # Properties
 ############
 func _get_property_list() -> Array:
-	var property_list: Array
+	var property_list: Array[Dictionary]
 
 	######################
 	# General - Properties
 	######################
 	property_list.append({
-		"name": _priority_property_name,
+		"name": PhantomCameraConstants.PRIORITY_PROPERTY_NAME,
 		"type": TYPE_INT,
 		"hint": PROPERTY_HINT_NONE,
 		"usage": PROPERTY_USAGE_DEFAULT
@@ -97,15 +43,15 @@ func _get_property_list() -> Array:
 	# Follow - Properties
 	#####################
 	property_list.append({
-		"name": _follow_target_property_name,
+		"name": PhantomCameraConstants.FOLLOW_TARGET_PROPERTY_NAME,
 		"type": TYPE_NODE_PATH,
 		"hint": PROPERTY_HINT_NONE,
 		"usage": PROPERTY_USAGE_DEFAULT
 	})
 
-	if has_follow_target:
+	if Phantom_Camera_Variables.has_follow_target:
 		property_list.append({
-			"name": _follow_target_offset_property_name,
+			"name": PhantomCameraConstants.FOLLOW_TARGET_OFFSET_PROPERTY_NAME,
 			"type": TYPE_VECTOR3,
 			"hint": PROPERTY_HINT_NONE,
 			"usage": PROPERTY_USAGE_DEFAULT
@@ -133,33 +79,33 @@ func _get_property_list() -> Array:
 	# Tween - Properties
 	####################
 	property_list.append({
-		"name": _tween_duration_property_name,
+		"name": PhantomCameraConstants.TWEEN_DURATION_PROPERTY_NAME,
 		"type": TYPE_FLOAT,
 		"hint": PROPERTY_HINT_NONE,
 		"usage": PROPERTY_USAGE_DEFAULT
 	})
 
 	property_list.append({
-		"name": _tween_transition_property_name,
+		"name": PhantomCameraConstants.TWEEN_TRANSITION_PROPERTY_NAME,
 		"type": TYPE_NIL,
 		"hint_string": "Transition_",
 		"usage": PROPERTY_USAGE_GROUP
 	})
 
 	property_list.append({
-		"name": _tween_transition_property_name,
+		"name": PhantomCameraConstants.TWEEN_TRANSITION_PROPERTY_NAME,
 		"type": TYPE_INT,
 		"hint": PROPERTY_HINT_ENUM,
-		"hint_string": ",".join(PackedStringArray(TweenTransitions.keys())),
+		"hint_string": ",".join(PackedStringArray(PhantomCameraConstants.TweenTransitions.keys())),
 		"usage": PROPERTY_USAGE_DEFAULT
 	})
 
 	if not tween_linear:
 		property_list.append({
-			"name": _tween_ease_property_name,
+			"name": PhantomCameraConstants.TWEEN_EASE_PROPERTY_NAME,
 			"type": TYPE_INT,
 			"hint": PROPERTY_HINT_ENUM,
-			"hint_string": ",".join(PackedStringArray(TweenEases.keys())),
+			"hint_string": ",".join(PackedStringArray(PhantomCameraConstants.TweenEases.keys())),
 			"usage": PROPERTY_USAGE_DEFAULT
 		})
 	return property_list
@@ -169,25 +115,25 @@ func _set(property: StringName, value) -> bool:
 	######################
 	# General - Properties
 	######################
-	if property == _priority_property_name:
+	if property == PhantomCameraConstants.PRIORITY_PROPERTY_NAME:
 		set_priority(value)
 
 	#####################
 	# Follow - Properties
 	#####################
-	if property == _follow_target_property_name:
-		_follow_target_path = value
+	if property == PhantomCameraConstants.FOLLOW_TARGET_PROPERTY_NAME:
+		Phantom_Camera_Variables.follow_target_path = value
 		var valueNodePath: NodePath = value as NodePath
 		if not valueNodePath.is_empty():
-			has_follow_target = true
-			if has_node(_follow_target_path):
-				follow_target_node = get_node(_follow_target_path)
+			Phantom_Camera_Variables.has_follow_target = true
+			if has_node(Phantom_Camera_Variables.follow_target_path):
+				Phantom_Camera_Variables.follow_target_node = get_node(Phantom_Camera_Variables.follow_target_path)
 		else:
-			has_follow_target = false
-			follow_target_node = null
+			Phantom_Camera_Variables.has_follow_target = false
+			Phantom_Camera_Variables.follow_target_node = null
 
 		notify_property_list_changed()
-	if property == _follow_target_offset_property_name:
+	if property == PhantomCameraConstants.FOLLOW_TARGET_OFFSET_PROPERTY_NAME:
 		if value == Vector3.ZERO:
 			printerr("Follow Offset cannot be 0,0,0, resetting to 0,0,1")
 			follow_target_offset = Vector3(0,0,1)
@@ -215,48 +161,49 @@ func _set(property: StringName, value) -> bool:
 	####################
 	# Tween - Properties
 	####################
-	if property == _tween_duration_property_name:
-		tween_duration = value
-	if property == _tween_transition_property_name:
+	if property == PhantomCameraConstants.TWEEN_DURATION_PROPERTY_NAME:
+		Phantom_Camera_Variables.tween_duration = value
+	if property == PhantomCameraConstants.TWEEN_TRANSITION_PROPERTY_NAME:
 		tween_linear = false
 		match value:
 			Tween.TRANS_LINEAR:
-				tween_transition = Tween.TRANS_LINEAR
+				Phantom_Camera_Variables.tween_transition = Tween.TRANS_LINEAR
 				tween_linear = true # Disables Easing property as it has no effect on Linear transitions
-			Tween.TRANS_BACK: 		tween_transition = Tween.TRANS_BACK
-			Tween.TRANS_SINE: 		tween_transition = Tween.TRANS_SINE
-			Tween.TRANS_QUINT: 		tween_transition = Tween.TRANS_QUINT
-			Tween.TRANS_QUART: 		tween_transition = Tween.TRANS_QUART
-			Tween.TRANS_QUAD: 		tween_transition = Tween.TRANS_QUAD
-			Tween.TRANS_EXPO: 		tween_transition = Tween.TRANS_EXPO
-			Tween.TRANS_ELASTIC: 	tween_transition = Tween.TRANS_ELASTIC
-			Tween.TRANS_CUBIC:		tween_transition = Tween.TRANS_CUBIC
-			Tween.TRANS_BOUNCE: 	tween_transition = Tween.TRANS_BOUNCE
-			Tween.TRANS_BACK: 		tween_transition = Tween.TRANS_BACK
+			Tween.TRANS_BACK: 		Phantom_Camera_Variables.tween_transition = Tween.TRANS_BACK
+			Tween.TRANS_SINE: 		Phantom_Camera_Variables.tween_transition = Tween.TRANS_SINE
+			Tween.TRANS_QUINT: 		Phantom_Camera_Variables.tween_transition = Tween.TRANS_QUINT
+			Tween.TRANS_QUART: 		Phantom_Camera_Variables.tween_transition = Tween.TRANS_QUART
+			Tween.TRANS_QUAD: 		Phantom_Camera_Variables.tween_transition = Tween.TRANS_QUAD
+			Tween.TRANS_EXPO: 		Phantom_Camera_Variables.tween_transition = Tween.TRANS_EXPO
+			Tween.TRANS_ELASTIC: 	Phantom_Camera_Variables.tween_transition = Tween.TRANS_ELASTIC
+			Tween.TRANS_CUBIC:		Phantom_Camera_Variables.tween_transition = Tween.TRANS_CUBIC
+			Tween.TRANS_BOUNCE: 	Phantom_Camera_Variables.tween_transition = Tween.TRANS_BOUNCE
+			Tween.TRANS_BACK: 		Phantom_Camera_Variables.tween_transition = Tween.TRANS_BACK
 			11:
 				print("Transition is custom")
-				tween_transition = 11
+				Phantom_Camera_Variables.tween_transition = 11
 		notify_property_list_changed()
-	if property == _tween_ease_property_name:
+	if property == PhantomCameraConstants.TWEEN_EASE_PROPERTY_NAME:
 		match value:
-			Tween.EASE_IN: 			tween_ease = Tween.EASE_IN
-			Tween.EASE_OUT: 		tween_ease = Tween.EASE_OUT
-			Tween.EASE_IN_OUT: 		tween_ease = Tween.EASE_IN_OUT
-			Tween.EASE_OUT_IN: 		tween_ease = Tween.EASE_OUT_IN
+			Tween.EASE_IN: 			Phantom_Camera_Variables.tween_ease = Tween.EASE_IN
+			Tween.EASE_OUT: 		Phantom_Camera_Variables.tween_ease = Tween.EASE_OUT
+			Tween.EASE_IN_OUT: 		Phantom_Camera_Variables.tween_ease = Tween.EASE_IN_OUT
+			Tween.EASE_OUT_IN: 		Phantom_Camera_Variables.tween_ease = Tween.EASE_OUT_IN
 	return false
 
 
 func _get(property: StringName):
+
 	######################
 	# General - Properties
 	######################
-	if property == _priority_property_name: return _priority
+	if property == PhantomCameraConstants.PRIORITY_PROPERTY_NAME: return Phantom_Camera_Variables.priority
 
 	#####################
 	# Follow - Properties
 	#####################
-	if property == _follow_target_property_name: return _follow_target_path
-	if property == _follow_target_offset_property_name: return follow_target_offset
+	if property == PhantomCameraConstants.FOLLOW_TARGET_PROPERTY_NAME: return Phantom_Camera_Variables.follow_target_path
+	if property == PhantomCameraConstants.FOLLOW_TARGET_OFFSET_PROPERTY_NAME: return follow_target_offset
 
 	######################
 	# Look At - Properties
@@ -267,51 +214,53 @@ func _get(property: StringName):
 	####################
 	# Tween - Properties
 	####################
-	if property == _tween_duration_property_name: return tween_duration
-	if property == _tween_transition_property_name: return tween_transition
-	if property == _tween_ease_property_name: return tween_ease
+	if property == PhantomCameraConstants.TWEEN_DURATION_PROPERTY_NAME: return Phantom_Camera_Variables.tween_duration
+	if property == PhantomCameraConstants.TWEEN_TRANSITION_PROPERTY_NAME: return Phantom_Camera_Variables.tween_transition
+	if property == PhantomCameraConstants.TWEEN_EASE_PROPERTY_NAME: return Phantom_Camera_Variables.tween_ease
 
 
 ##############
 # Private Functions
 ##############
 func _enter_tree() -> void:
-	add_to_group("phantom_camera_group")
+#	Phantom_Camera_Variables = PhantomCameraVariables.new()
 
-	camera_base_group = get_tree().get_nodes_in_group(PHANTOM_CAMERA_BASE_GROUP_NAME)
+	add_to_group(PhantomCameraConstants.PHANTOM_CAMERA_GROUP_NAME)
 
-	if camera_base_group.size() > 0:
-		if camera_base_group.size() == 1:
-			phantom_camera_base_owner = camera_base_group[0]
-			phantom_camera_base_owner.phantom_camera_added_to_scene(self)
+	Phantom_Camera_Variables.camera_host_group = get_tree().get_nodes_in_group(PhantomCameraConstants.PHANTOM_CAMERA_HOST_GROUP_NAME)
+
+	if Phantom_Camera_Variables.camera_host_group.size() > 0:
+		if Phantom_Camera_Variables.camera_host_group.size() == 1:
+			phantom_camera_host_owner = Phantom_Camera_Variables.camera_host_group[0]
+			phantom_camera_host_owner.phantom_camera_added_to_scene(self)
 			pass
 		else:
-			for camera_base in camera_base_group:
+			for camera_host in Phantom_Camera_Variables.camera_host_group:
 				print("Multiple PhantomCameraBases in scene")
 	else:
 		print("No camera base added")
 
-	phantom_camera_base_owner.phantom_camera_added_to_scene(self)
+	phantom_camera_host_owner.phantom_camera_added_to_scene(self)
 
 	if _look_at_target_path:
 		look_at_target_node = get_node(_look_at_target_path)
 
-	if _follow_target_path:
-		follow_target_node = get_node(_follow_target_path)
+	if Phantom_Camera_Variables.follow_target_path:
+		Phantom_Camera_Variables.follow_target_node = get_node(Phantom_Camera_Variables.follow_target_path)
 
 
 func _exit_tree() -> void:
-	phantom_camera_base_owner.phantom_camera_removed_from_scene(self)
+	phantom_camera_host_owner.phantom_camera_removed_from_scene(self)
 	print("phantom_camera leaving tree")
 
 
 func _physics_process(delta: float) -> void:
-	if follow_target_node:
+	if Phantom_Camera_Variables.follow_target_node:
 
 #		if camera_smoothing == 0:
-			set_position(
-				follow_target_node.position + follow_target_offset
-			)
+		set_position(
+			Phantom_Camera_Variables.follow_target_node.position + follow_target_offset
+		)
 #		else:
 #			# TODO - Change camera_smoothing value to something more sensible in the editor
 #			set_position(
@@ -328,15 +277,21 @@ func _physics_process(delta: float) -> void:
 ##################
 func set_priority(value: int) -> void:
 	if value < 1:
-		_priority = 1
+		Phantom_Camera_Variables.priority = 1
 	else:
-		_priority = value
+		Phantom_Camera_Variables.priority = value
 
-	if phantom_camera_base_owner:
-		phantom_camera_base_owner.phantom_camera_priority_updated(self)
+	if phantom_camera_host_owner:
+		phantom_camera_host_owner.phantom_camera_priority_updated(self)
 	else:
-#			TODO - Add logic to handle Phantom Camera Base in scene
+#			TODO - Add logic to handle Phantom Camera Host in scene
 		pass
 
 func get_priority() -> int:
-	return _priority
+	return Phantom_Camera_Variables.priority
+
+func get_tween_duration() -> float:
+	return Phantom_Camera_Variables.tween_duration
+
+func get_tween_transition() -> int:
+	return Phantom_Camera_Variables.tween_transition
