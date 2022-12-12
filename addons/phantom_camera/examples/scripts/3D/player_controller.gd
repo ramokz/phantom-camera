527d2d6a1a1f1d6ad26d2deefc7c7a6c920c6f8f
Marcus <hello@marcusskov.com>
2022-12-12 23:41:41 +0000

    Updated player_controller.gd in 3D demo scene with WASD physical keys input

--- addons/phantom_camera/examples/scripts/3D/player_controller.gd
+++ addons/phantom_camera/examples/scripts/3D/player_controller.gd
@@ -7,10 +7,43 @@ extends CharacterBody3D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var left_movement: StringName
+const KEY_STRINGNAME: StringName = "Key"
+const ACTION_STRINGNAME: StringName = "Action"
 
-#func _enter_tree() -> void:
-#	left_movement = ""
+const INPUT_MOVE_UP_STRINGNAME: StringName = "move_up"
+const INPUT_MOVE_DOWM_STRINGNAME: StringName = "move_down"
+const INPUT_MOVE_LEFT_STRINGNAME: StringName = "move_left"
+const INPUT_MOVE_RIGHT_STRINGNAME: StringName = "move_right"
+
+var InputMovementDic: Dictionary = {
+	INPUT_MOVE_UP_STRINGNAME: {
+		KEY_STRINGNAME: KEY_W,
+		ACTION_STRINGNAME: INPUT_MOVE_UP_STRINGNAME
+	},
+	INPUT_MOVE_DOWM_STRINGNAME: {
+		KEY_STRINGNAME: KEY_S,
+		ACTION_STRINGNAME: INPUT_MOVE_DOWM_STRINGNAME
+	},
+	INPUT_MOVE_LEFT_STRINGNAME: {
+		KEY_STRINGNAME: KEY_A,
+		ACTION_STRINGNAME: INPUT_MOVE_LEFT_STRINGNAME
+	},
+	INPUT_MOVE_RIGHT_STRINGNAME: {
+		KEY_STRINGNAME: KEY_D,
+		ACTION_STRINGNAME: INPUT_MOVE_RIGHT_STRINGNAME
+	},
+}
+
+
+func _ready() -> void:
+	for input in InputMovementDic:
+		var key_val = InputMovementDic[input].get(KEY_STRINGNAME)
+		var action_val = InputMovementDic[input].get(ACTION_STRINGNAME)
+
+		var movement_input = InputEventKey.new()
+		movement_input.physical_keycode = key_val
+		InputMap.add_action(action_val)
+		InputMap.action_add_event(action_val, movement_input)
 
 
 func _physics_process(delta: float) -> void:
@@ -20,8 +53,13 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir : Vector2 = Input.get_vector(
+		INPUT_MOVE_LEFT_STRINGNAME,
+		INPUT_MOVE_RIGHT_STRINGNAME,
+		INPUT_MOVE_UP_STRINGNAME,
+		INPUT_MOVE_DOWM_STRINGNAME
+	)
+	var direction : Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
