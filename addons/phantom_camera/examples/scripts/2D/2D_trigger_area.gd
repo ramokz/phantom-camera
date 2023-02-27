57d8dc7d3e95d35ee8a0cc896ec77d44e6a02e29
Marcus <hello@marcusskov.com>
2023-02-27 21:42:41 +0000

    New & Updated trigger areas scripts - [Renamed] trigger_area.gd to 3D_trigger_area.gd for 3D scene - [Added] 2D_trigger_area.gd for 2D scene - [Added] 2D Tweening example scene

---
+++ addons/phantom_camera/examples/scripts/2D/2D_trigger_area.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var area_pcam: PhantomCamera2D
+
+func _ready() -> void:
+	connect("area_entered", _entered_area)
+	connect("area_exited", _exited_area)
+
+func _entered_area(area_2D: Area2D) -> void:
+	if area_2D.get_parent() is CharacterBody2D:
+		area_pcam.set_priority(20)
+
+func _exited_area(area_2D: Area2D) -> void:
+	if area_2D.get_parent() is CharacterBody2D:
+		area_pcam.set_priority(0)
+
