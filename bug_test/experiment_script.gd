a69af6770464f56c29110905674da68c83d1edfe
Marcus <hello@marcusskov.com>
2023-09-14 20:59:09 +0100

    Minor changes to Bug Test Scene - [Removed] SpringArm.gd

--- bug_test/experiment_script.gd
+++ bug_test/experiment_script.gd
@@ -1,40 +1,9 @@
-#@tool
+@tool
 extends Node3D
 
-@export var target: Node3D
+@onready var player: Node3D = %PlayerCharacterBody3D
 
-var distance: float = 2
-var theta: float = PI / 4
-
-
-func _process(delta):
-#	var offset = Vector3(2, 0, 0)
-#
-#	var pivot_point: Vector3 = target.global_position
-#	print(global_position)
-
-#	print(get_tree().)
-
-#	global_position = Vector3 (
-#		sin(theta) * distance,
-#		position.y,
-#		cos(theta) * distance,
-#	) + Vector3(-1, 0, -2)
-##
-#	theta += delta
-
-
-
-#	transform = pi
-
-#	angle+= delta
-
-#	theta = atan()
-
-
-
-#	global_transform = target.global_transform * Transform3D().rotated(Vector3.ZERO.normalized(), angle)
-
-
-
-	pass
+func _input(event: InputEvent):
+	if event is InputEventKey and event.is_pressed():
+		if event.keycode == KEY_SPACE:
+			print("Is space")
