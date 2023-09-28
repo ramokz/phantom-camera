6a88ab5a47bdadf64a3e5665c4f301658ef09aae
Marcus <hello@marcusskov.com>
2023-09-28 23:34:01 +0100

    Updated Third Person Example scene - [Added] Aim PCam to toggle between - [Added] Mohawk to character to better tell which direction it's facing - [Updated] Playerable character to turn in the direction the camera is pointing at when moving

--- addons/phantom_camera/examples/scripts/3D/player_controller.gd
+++ addons/phantom_camera/examples/scripts/3D/player_controller.gd
@@ -1,14 +1,24 @@
 extends CharacterBody3D
 
-
 @export var SPEED = 5.0
 @export var JUMP_VELOCITY = 4.5
 @export var enable_gravity = true
 
 @onready var _camera: Camera3D = %MainCamera3D
+@onready var _player_pcam: PhantomCamera3D = %PlayerPhantomCamera3D
+@onready var _aim_pcam: PhantomCamera3D = %PlayerAimPhantomCamera3D
+@onready var _model: Node3D = $PlayerModel
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = 9.8
+
+@export var mouse_sensitivity: float = 0.05
+
+@export var min_yaw: float = -89.9
+@export var max_yaw: float = 50
+
+@export var min_pitch: float = 0
+@export var max_pitch: float = 360
 
 const KEY_STRINGNAME: StringName = "Key"
 const ACTION_STRINGNAME: StringName = "Action"
@@ -47,6 +57,9 @@ func _ready() -> void:
 		movement_input.physical_keycode = key_val
 		InputMap.add_action(action_val)
 		InputMap.action_add_event(action_val, movement_input)
+		
+		if _player_pcam.get_follow_mode() == _player_pcam.Constants.FollowMode.THIRD_PERSON:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 func _physics_process(delta: float) -> void:
@@ -79,3 +92,43 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	if velocity.length() > 0.2:
+		var look_direction: Vector2 = Vector2(velocity.z, velocity.x)
+		_model.rotation.y = look_direction.angle()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if _player_pcam.get_follow_mode() == _player_pcam.Constants.FollowMode.THIRD_PERSON:
+		var active_pcam: PhantomCamera3D
+		
+		if is_instance_valid(_aim_pcam):
+			_set_pcam_rotation(_player_pcam, event)
+			_set_pcam_rotation(_aim_pcam, event)
+			if _player_pcam.get_priority() > _aim_pcam.get_priority():
+#				active_pcam = _player_pcam
+				_toggle_aim_pcam(event)
+			else:
+#				_set_pcam_rotation(_aim_pcam, event)
+				_toggle_aim_pcam(event)
+
+
+func _set_pcam_rotation(pcam: PhantomCamera3D, event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var pcam_rotation_degrees: Vector3
+		pcam_rotation_degrees = pcam.get_third_person_rotation_degrees()
+		pcam_rotation_degrees.x -= event.relative.y * mouse_sensitivity
+		pcam_rotation_degrees.x = clamp(pcam_rotation_degrees.x, min_yaw, max_yaw)
+
+		pcam_rotation_degrees.y -= event.relative.x * mouse_sensitivity
+		pcam_rotation_degrees.y = wrapf(pcam_rotation_degrees.y, min_pitch, max_pitch)
+	
+		pcam.set_third_person_rotation_degrees(pcam_rotation_degrees)
+
+
+func _toggle_aim_pcam(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == 2:
+		if _player_pcam.get_priority() > _aim_pcam.get_priority():
+			_aim_pcam.set_priority(30)
+		else:
+			_aim_pcam.set_priority(0)
