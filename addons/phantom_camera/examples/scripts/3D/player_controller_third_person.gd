23da8a19d4ef5ef6b27bf4d12bbd113c2cb74da8
Marcus <hello@marcusskov.com>
2023-10-08 12:16:37 +0100

    Minor comment additions in the player_controller_third_person.gd

--- addons/phantom_camera/examples/scripts/3D/player_controller_third_person.gd
+++ addons/phantom_camera/examples/scripts/3D/player_controller_third_person.gd
@@ -52,13 +52,23 @@ func _unhandled_input(event: InputEvent) -> void:
 func _set_pcam_rotation(pcam: PhantomCamera3D, event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var pcam_rotation_degrees: Vector3
+		
+		# Assigns the current 3D rotation of the SpringArm3D node - so it starts off where it is in the editor 
 		pcam_rotation_degrees = pcam.get_third_person_rotation_degrees()
+		
+		# Change the X rotation
 		pcam_rotation_degrees.x -= event.relative.y * mouse_sensitivity
-		pcam_rotation_degrees.x = clamp(pcam_rotation_degrees.x, min_yaw, max_yaw)
+		
+		# Clamp the rotation in the X axis so it go over or under the target
+		pcam_rotation_degrees.x = clampf(pcam_rotation_degrees.x, min_yaw, max_yaw)
 
+		# Change the Y rotation value
 		pcam_rotation_degrees.y -= event.relative.x * mouse_sensitivity
+		
+		# Sets the rotation to fully loop around its target, but witout going below or exceeding 0 and 360 degrees
 		pcam_rotation_degrees.y = wrapf(pcam_rotation_degrees.y, min_pitch, max_pitch)
-	
+		
+		# Change the SpringArm3D node's rotation and rotate around its target
 		pcam.set_third_person_rotation_degrees(pcam_rotation_degrees)
 
 
