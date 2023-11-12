78e0513b64ef822c4c4a51245cac29bb6c0efc17
Marcus <hello@marcusskov.com>
2023-11-12 23:37:12 +0000

    Registered Methods components globally

--- docs/docs/.vitepress/theme/index.ts
+++ docs/docs/.vitepress/theme/index.ts
@@ -5,14 +5,19 @@ import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import './custom.css'
 
+import MethodsComponent from "../../components/methods/MethodsComponent.vue";
+import MethodsSetGet from "../../components/methods/MethodsSetGet.vue";
+
 export default {
   extends: DefaultTheme,
+  async enhanceApp({ app }) {
+    // register your custom global components
+    app.component('MethodsComponent', MethodsComponent),
+    app.component('MethodsSetGet', MethodsSetGet)
+  },
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
 } satisfies Theme
