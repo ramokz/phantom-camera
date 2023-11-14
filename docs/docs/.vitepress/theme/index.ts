17001a499c3f5928193a86f412145a904c8721d3
Marcus <hello@marcusskov.com>
2023-11-14 23:41:25 +0000

    Updated component names in index.ts

--- docs/docs/.vitepress/theme/index.ts
+++ docs/docs/.vitepress/theme/index.ts
@@ -5,15 +5,15 @@ import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import './custom.css'
 
-import MethodsComponent from "../../components/methods/MethodsComponent.vue";
-import MethodsSetGet from "../../components/methods/MethodsSetGet.vue";
+import MethodComponent from "../../components/methods/MethodComponent.vue";
+import MethodSetGet from "../../components/methods/MethodSetGet.vue";
 
 export default {
   extends: DefaultTheme,
   async enhanceApp({ app }) {
     // register your custom global components
-    app.component('MethodsComponent', MethodsComponent),
-    app.component('MethodsSetGet', MethodsSetGet)
+    app.component('MethodComponent', MethodComponent),
+    app.component('MethodSetGet', MethodSetGet)
   },
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
