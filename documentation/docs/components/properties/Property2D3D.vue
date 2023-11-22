0a55e84e7c91a0290397239f2431cf9fdc958407
Marcus <hello@marcusskov.com>
2023-11-22 22:27:40 +0000

    Replaced hardcoded 2D/3D tabs with component version.

--- documentation/docs/components/properties/Property2D3D.vue
+++ documentation/docs/components/properties/Property2D3D.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import {ref, useSlots, computed} from 'vue'
 import { store } from '../../store.ts'
-  
+
+import Tabs2D3D from "../Tabs2D3D.vue";
+
   const props = defineProps({
     propertyName: {
       type: String,
@@ -55,26 +57,10 @@ import { store } from '../../store.ts'
   const hasPropertyType = (typeArray: Array<string>) => {
     return slotContent.some(value => typeArray.includes(value))
   }
-  
-  const propertyNameID = computed(() => {
-    return props.propertyName.replace(/ /g, '')
-  })
-
 </script>
 
 <template>
-  <div class="toggle-tabs">
-      <input type="radio" :id="propertyNameID + '2D'" :name="propertyNameID" :value="store.is2D" v-model="store.is2D" :checked="store.is2D" @change="store.toggle2D(true)">
-      <label :for="propertyNameID + '2D'" @mouseenter="setCornerRadius(true)" @mouseleave="setCornerRadius(false)">
-        <img alt="Phantom Camera 2D" src="../../assets/icons/phantom-camera-2D.svg" width="32"/>
-        2D
-      </label>
-      <input type="radio" :id="propertyNameID + '3D'" :name="propertyNameID" :value="!store.is2D" v-model="store.is2D" :checked="!store.is2D" @change="store.toggle2D(false)">
-      <label :for="propertyNameID + '3D'">
-        <img alt="Phantom Camera 3D" src="../../assets/icons/phantom-camera-3D.svg" width="32"/>
-        3D
-      </label>
-  </div>
+  <Tabs2D3D :property-name="propertyName"/>
   <Property :style="{borderRadius: borderRadius}" :propertyName="propertyName" :propertyType="propertyObj.type" :propertyDefault="propertyObj.default">
     <template #propertyDescription>
       <slot name="propertyDescription"/>
@@ -130,38 +116,3 @@ import { store } from '../../store.ts'
     </template>
   </Property>
 </template>
-
-<style scoped>
-.toggle-tabs {
-  display: flex;
-  input {
-    margin: 20px;
-    appearance: none;
-    display: none;
-    & + label {
-      cursor: pointer;
-      font-size: 18px;
-      font-weight: 800;
-      display: flex;
-      gap: 6px;
-      align-items: center;
-      padding: 6px 12px 6px 12px;
-      border-radius: 12px 12px 0 0;
-      opacity: 0.8;
-      border: 2px solid transparent;
-      margin-bottom: -2px;
-      &:hover {
-        box-shadow: 0 0 30px var(--vp-c-bg-alt) inset;
-        border-color: var(--vp-c-brand-1);
-      }
-    }
-    &:checked + label {
-      opacity: 1;
-      background: var(--vp-c-bg-alt);
-      box-shadow: 0px 0px 10px var(--vp-c-bg) inset;
-      border: 2px solid var(--vp-c-gray-3);
-      z-index: -10;
-    }
-  }
-}
-</style>
