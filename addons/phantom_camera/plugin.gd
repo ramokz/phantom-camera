1a349da4a0572230f91e3f718ddbbcdfc8126de4
Marcus Skov <Hello@MarcusSkov.com>
2024-02-21 21:17:22 +0000

    Viewfinder 2d (#220)
    * Initial working 2D viewfinder
    
    * Updated gizmo icon
    - [Removed] Old Gizmo icon(s)
    - [Updated] viewfinder_panel.tscn with improved icon sizing layout
    
    * Improved 2D viewfinder
    - [Renamed] Lowercased ...2D and ...3D to 2d/3d in various variable names
    
    * Improved aspect ratio scaler
    
    * Improved viewfinder logic checker
    
    * Removed commented out logic
    
    * CleanED up viewfinder file
    
    - [Removed] editor_interface variables and replaced with singleton Class ref
    - [Removed] Unneeded conditional checker and logic
    
    * Simplified scene change signal function
    
    * Solved error with viewfinder
     Now no longer being referenced from export builds
    
    * Updated example scenes

--- addons/phantom_camera/plugin.gd
+++ addons/phantom_camera/plugin.gd
@@ -18,7 +18,8 @@ const EditorPanel = preload("res://addons/phantom_camera/panel/editor.tscn")
 
 var pcam_3D_gizmo_plugin = Pcam3DPlugin.new()
 
-var editor_panel_instance
+var editor_panel_instance: Control
+var panel_button: Button
 #var viewfinder_panel_instance
 
 #endregion
@@ -29,20 +30,30 @@ var editor_panel_instance
 func _enter_tree() -> void:
 	# Phantom Camera Nodes
 	add_custom_type(PCAM_2D, "Node2D", preload("res://addons/phantom_camera/scripts/phantom_camera/phantom_camera_2D.gd"), preload("res://addons/phantom_camera/icons/phantom_camera_2d.svg"))
-	add_custom_type(PCAM_3D, "Node3D", preload("res://addons/phantom_camera/scripts/phantom_camera/phantom_camera_3D.gd"), preload("res://addons/phantom_camera/icons/phantom_camera_3d.svg"))
-	add_custom_type(PCAM_HOST, "Node", preload("res://addons/phantom_camera/scripts/phantom_camera_host/phantom_camera_host.gd"), preload("res://addons/phantom_camera/icons/phantom_camera_host.svg"))
+	add_custom_type(PCAM_3D, "Node3D", preload("res://addons/phantom_camera/scripts/phantom_camera/phantom_camera_3D.gd"), preload("res://addons/phantom_camera/icons/phantom_camera_2d.svg"))
+	add_custom_type(PCAM_HOST, "Node", preload("res://addons/phantom_camera/scripts/phantom_camera_host/phantom_camera_host.gd"), preload("res://addons/phantom_camera/icons/phantom_camera_2d.svg"))
 
 	# Phantom Camera 3D Gizmo
 	add_node_3d_gizmo_plugin(pcam_3D_gizmo_plugin)
 
+	# TODO - Should be disabled unless in editor
 	# Viewfinder
 	editor_panel_instance = EditorPanel.instantiate()
-	editor_panel_instance.editor_interface = get_editor_interface()
 	editor_panel_instance.editor_plugin = self
-	add_control_to_bottom_panel(editor_panel_instance, "Phantom Camera")
-	_make_visible(false)
+	panel_button = add_control_to_bottom_panel(editor_panel_instance, "Phantom Camera")
+	
+	# Trigger events in the viewfinder whenever 
+	panel_button.toggled.connect(btn_toggled)
+	
+	scene_changed.connect(editor_panel_instance.viewfinder.scene_changed)
 
-	connect("scene_changed", _scene_changed)
+
+func btn_toggled(toggled_on: bool):
+	if toggled_on:
+		editor_panel_instance.viewfinder.viewfinder_visible = true
+		editor_panel_instance.viewfinder.visibility_check()
+	else:
+		editor_panel_instance.viewfinder.viewfinder_visible = false
 
 
 func _exit_tree() -> void:
@@ -54,21 +65,6 @@ func _exit_tree() -> void:
 
 	remove_control_from_bottom_panel(editor_panel_instance)
 	editor_panel_instance.queue_free()
-#	if framed_viewfinder_panel_instance:
-	disconnect("scene_changed", _scene_changed)
-
-
-#func _has_main_screen():
-#	return true;
-
-
-func _make_visible(visible):
-	if editor_panel_instance:
-		editor_panel_instance.set_visible(visible)
-
-
-func _scene_changed(scene_root: Node) -> void:
-	editor_panel_instance.viewfinder.scene_changed(scene_root)
 
 #endregion
 
