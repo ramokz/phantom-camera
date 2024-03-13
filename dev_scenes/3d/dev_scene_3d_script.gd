e3c67828f6670ac0402d4afedd9c44d177c638f4
Marcus <hello@marcusskov.com>
2024-03-13 21:45:07 +0000

    Updated 3D dev scene and script

--- dev_scenes/3d/dev_scene_3d_script.gd
+++ dev_scenes/3d/dev_scene_3d_script.gd
@@ -1,19 +1,16 @@
 extends Node3D
 
-@onready var pcam_3d: PhantomCamera3D = %PhantomCamera3D 
-@onready var pcam_2_3d: PhantomCamera3D = %PhantomCamera3D2
+@onready var pcam: PhantomCamera3D = %ScenePhantomCamera3D
+@onready var player: CharacterBody3D = %PlayerCharacterBody3D2
+@onready var player_pcam: PhantomCamera3D = %PlayerPhantomCamera3D
 
 
-func _ready():
-	#pcam_2_3d.set_camera_fov(10)
-	#print(pcam_2_3d.get_camera_fov())
-	pass
 
 
 func _unhandled_input(event):
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_SPACE:
-			if pcam_2_3d.get_priority() < 30 and pcam_3d.is_active():
-				pcam_2_3d.set_priority(30)
+			if player_pcam.get_priority() < 30:
+				player_pcam.set_priority(30)
 			else:
-				pcam_2_3d.set_priority(0)
+				player_pcam.set_priority(0)
