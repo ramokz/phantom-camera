29a7770a8f3fdd98b2aebb5e364498d24e76f4e5
Marcus <hello@marcusskov.com>
2024-05-20 12:41:42 +0100

    Set _player_visual to top level in 2D example scene

--- addons/phantom_camera/examples/scripts/2D/player_character_body_2d.gd
+++ addons/phantom_camera/examples/scripts/2D/player_character_body_2d.gd
@@ -49,7 +49,7 @@ var InputMovementDic: Dictionary = {
 func _ready() -> void:
 	_player_area2d.connect("body_shape_entered", _show_prompt)
 	_player_area2d.connect("body_shape_exited", _hide_prompt)
-	
+
 	_ui_sign = owner.get_node("%UISign")
 
 	for input in InputMovementDic:
@@ -60,6 +60,8 @@ func _ready() -> void:
 		movement_input.physical_keycode = key_val
 		InputMap.add_action(action_val)
 		InputMap.action_add_event(action_val, movement_input)
+
+	_player_visuals.top_level = true
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -109,7 +111,6 @@ func _interactive_node_logic() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	
 	_physics_body_trans_last = _physics_body_trans_current
 	_physics_body_trans_current = global_transform
 
@@ -143,7 +144,7 @@ func _process(delta) -> void:
 		_physics_body_trans_current,
 		Engine.get_physics_interpolation_fraction()
 	).origin
-	
+
 
 func _show_prompt(body_rid: RID, body: Node2D, body_shape_index: int, local_shape: int) -> void:
 	if body is TileMap:
