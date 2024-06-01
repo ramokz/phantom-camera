6e0670d695d0f4d645739992a4c2148dde66dafe
Marcus <hello@marcusskov.com>
2024-06-01 20:37:29 +0100

    Added return and parameter type to 3D dev scene

--- dev_scenes/3d/scripts/dev_scene_3d_script.gd
+++ dev_scenes/3d/scripts/dev_scene_3d_script.gd
@@ -4,7 +4,7 @@ extends Node3D
 @onready var player_pcam: PhantomCamera3D = %PlayerPhantomCamera3D
 
 
-func _unhandled_input(event):
+func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_SPACE:
 			if player_pcam.priority < 30:
