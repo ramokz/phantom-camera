70f5b66c9d5e2f6a6c33fe15e5d708e62a5bbeb5
Cody Bentley <cody.bentley.la@gmail.com>
2024-07-19 18:35:19 -0600

    (#286) Finished the C# API

--- dev_scenes/3d/scripts/DevSceneCSharp3D.cs
+++ dev_scenes/3d/scripts/DevSceneCSharp3D.cs
@@ -5,9 +5,8 @@ public partial class DevSceneCSharp3D : Node3D
 {
     public override void _Ready()
     {
-        var pCamNode = GetNode<Node3D>("Player/PlayerCam");
-        var pCam = pCamNode.AsPhantomCamera3D();
+        var pCam = GetNode<Node3D>("Player/PlayerCam").AsPhantomCamera3D();
         
-        GD.Print(pCam);
+        GD.Print(pCam.Node3D);
     }
 }
