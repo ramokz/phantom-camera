a72a43cc71f827571dd7d0a0b75056830b54eb92
Marcus <hello@marcusskov.com>
2024-10-29 11:43:33 +0000

    Simplified Third Person Follow checker in logic process

--- addons/phantom_camera/scripts/phantom_camera/phantom_camera_3d.gd
+++ addons/phantom_camera/scripts/phantom_camera/phantom_camera_3d.gd
@@ -469,6 +469,7 @@ var _follow_framed_initial_set: bool = false
 var _follow_framed_offset: Vector3
 
 var _follow_spring_arm: SpringArm3D
+var _has_follow_spring_arm: bool = false
 
 var _current_rotation: Vector3
 
@@ -703,6 +704,7 @@ func _ready():
 					_follow_spring_arm.add_excluded_object(follow_target)
 					get_parent().add_child.call_deferred(_follow_spring_arm)
 					reparent.call_deferred(_follow_spring_arm)
+					_has_follow_spring_arm = true
 		FollowMode.FRAMED:
 			if not Engine.is_editor_hint():
 				_follow_framed_offset = global_position - _get_target_position_offset()
@@ -864,9 +866,9 @@ func _follow(delta: float) -> void:
 
 		FollowMode.THIRD_PERSON:
 			if not Engine.is_editor_hint():
-				if is_instance_valid(follow_target) and is_instance_valid(_follow_spring_arm):
-					follow_position = _get_target_position_offset()
-					follow_target_node = _follow_spring_arm
+				if not _has_follow_spring_arm: return
+				follow_position = _get_target_position_offset()
+				follow_target_node = _follow_spring_arm
 			else:
 				follow_position = _get_position_offset_distance()
 
