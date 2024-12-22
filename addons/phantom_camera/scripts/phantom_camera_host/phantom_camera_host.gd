3a1a07d1c3b0b75c7f69534885214911a4750d7c
ramokz <hello@marcusskov.com>
2024-12-22 20:42:57 +0100

    Minor spacing correction

--- addons/phantom_camera/scripts/phantom_camera_host/phantom_camera_host.gd
+++ addons/phantom_camera/scripts/phantom_camera_host/phantom_camera_host.gd
@@ -281,7 +281,7 @@ func _check_camera_host_amount() -> void:
 
 func _assign_new_active_pcam(pcam: Node) -> void:
 	# Only checks if the scene tree is still present.
-    # Prevents a few errors and checks from happening if the scene is exited.
+	# Prevents a few errors and checks from happening if the scene is exited.
 	if not is_inside_tree(): return
 	var no_previous_pcam: bool
 	if is_instance_valid(_active_pcam_2d) or is_instance_valid(_active_pcam_3d):
