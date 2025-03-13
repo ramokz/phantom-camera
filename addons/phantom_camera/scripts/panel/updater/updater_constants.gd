48cae3ab88ff77b4e7c54c9224ec0090dd718ce8
Marcus <hello@marcusskov.com>
2025-03-13 23:54:07 +0000

    Removed deprecated updater string constants

--- addons/phantom_camera/scripts/panel/updater/updater_constants.gd
+++ addons/phantom_camera/scripts/panel/updater/updater_constants.gd
@@ -5,5 +5,4 @@ const setting_phantom_camera: StringName = "phantom_camera/"
 const setting_updater_name: StringName = setting_phantom_camera + "updater/"
 
 # Updater Settings
-const setting_updater_enabled: StringName = setting_updater_name + "enable_updater"
-const setting_updater_notify_release: StringName = setting_updater_name + "show_new_release_info_on_editor_launch_in_output"
+const setting_updater_mode: StringName = setting_updater_name + "updater_mode"
