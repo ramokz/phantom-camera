b1de927aa74430e85be97809790bd0c3c5d71b87
GabCoolGuy <gabrielfreville@proton.me>
2025-05-08 14:48:15 +0200

    C# Wrapper (#512)
    * WIP: Adding CSharp wrapper
    - Begin effort on PCam3D and some basic Godot extensions and PCam types
    
    * WIP: rough first pass at PhantomCamera with 2D and 3D classes
    
    * (#286) Finished the C# API
    
    * WIP #286: fixed bugs in c# wrapper and added tests
    - added Engine singleton registration to PhantomCameraManager
    - fixed incorrect LookAtMode signal (not available in 2d)
    - added c# project files
    - created basic test runner and added some tests (wip)
    
    * WIP #286: more tests and re-organized
    - fix several bugs from invalid getter/setters, properties, and types
    - added more tests
    - added LimitTarget query result type for working with TileMaps and CollisionShape2Ds
    - reorganized scripts since main script file had become quite large
    
    * wip: #286 C# Wrapper
    - added missing shared camera properties
    - added missing phantom camera 2d properties
    - added missing phantom camera 3d properties
    
    * WIP #268: catching up to release 0.7.3
    
    * WIP: Adding CSharp wrapper
    - Begin effort on PCam3D and some basic Godot extensions and PCam types
    
    * Fix addon not working correctly
    
    I hate merging manually
    
    * Implement PCamNoiseEmitter2D and PCamNoise2D
    
    * Add more tests, still unfinished
    
    * Address reviews
    
    * Finalize c# wrapper logic
    
    Added missing setters: if a variable had multiple setters, they were missing, so they were added.
    
    * Make all classes variant compatible
    
    * Revert "Make all classes variant compatible"
    
    This reverts commit 902b2f19c39f6244cbb5e68c1605fa758c2ff6be.
    
    * Partly address reviews
    
    - Removed dedicated dev scene for C# (and its script)
    - Removed junk files from the .csproj file
    - Fixed dev scene 3D having the wrong root node
    
    * Address remaining reviews
    
    - Removed ReSharper comment in PhantomCamera2D.cs
    - Added getters in PhantomCameraManager.cs
    - Removed the android and ios specific parts of the csproj
    
    * Remove left over doc comment
    
    * Revert dev_scene_3d.tscn to how it was before
    
    (idk why it was changed, it wasnt me, i just tried to fix it)
    
    * Add missing properties and methods from recent updates and address reviews
    
    - Added KeepAspect property and enum in Camera3DResource.cs
    - Removed PhantomCameraHostOwner property in  PhantomCamera.cs
    - Added TeleportPosition to PhantomCamera2D.cs
    - Replace Rotation properties with setters and getters in PhantomCamera3D.cs
    - Added missing methods in PhantomCameraNoiseEmitter2D.cs and PhantomCameraNoiseEmitter3D.cs
    - Made enum types match their GDScript counter-part in PhantomCameraTween.cs
    
    * Address reviews
    
    - Removed "Rotation" from GetThirdPersonQuaternion and its setter
    - Gave GetThirdPersonQuaternion and its setter the correct MethodName
    - Added LimitTarget property in PhantomCamera2D.cs
    - Removed constant floats from Camera3DResource.cs
    
    ---------
    
    Co-authored-by: Cody Bentley <cody.bentley.la@gmail.com>

---
+++ addons/phantom_camera/scripts/phantom_camera_host/PhantomCameraHost.cs
@@ -0,0 +1,91 @@
+﻿using Godot;
+
+#nullable enable
+
+namespace PhantomCamera;
+
+// public enum InterpolationMode
+// {
+//     Auto,
+//     Idle,
+//     Physics
+// }
+
+public static class PhantomCameraHostExtensions
+{
+    public static PhantomCameraHost AsPhantomCameraHost(this Node node)
+    {
+        return new PhantomCameraHost(node);
+    }
+}
+
+public class PhantomCameraHost(Node node)
+{
+    
+    public Node Node { get; } = node;
+
+    // For when Godot becomes the minimum version
+    // public InterpolationMode InterpolationMode
+    // {
+    //      get => (InterpolationMode)(int)Node.Call(MethodName.GetInterpolationMode);
+    //      set => Node.Call(MethodName.SetInterpolationMode, (int)value);
+    // }
+    
+    public int HostLayers
+    {
+        get => (int)Node.Call(PhantomCamera.MethodName.GetHostLayers);
+        set => Node.Call(PhantomCamera.MethodName.SetHostLayers, value);
+    }
+
+    public void SetHostLayersValue(int layer, bool value) => Node.Call(MethodName.SetHostLayersValue, layer, value);
+
+    public Camera2D? Camera2D => (Camera2D?)Node.Get(PropertyName.Camera2D);
+
+    public Camera3D? Camera3D => (Camera3D?)Node.Get(PropertyName.Camera3D);
+
+    public bool TriggerPhantomCameraTween => (bool)Node.Call(MethodName.GetTriggerPhantomCameraTween);
+
+    public ActivePhantomCameraQueryResult? GetActivePhantomCamera()
+    {
+        var result = Node.Call(MethodName.GetActivePhantomCamera);
+        return result.VariantType == Variant.Type.Nil ? null : new ActivePhantomCameraQueryResult(result.AsGodotObject());
+    }
+
+    public static class PropertyName
+    {
+        public const string Camera2D = "camera_2d";
+        public const string Camera3D = "camera_3d";
+    }
+    
+    public static class MethodName
+    {
+        public const string GetActivePhantomCamera = "get_active_pcam";
+        public const string GetTriggerPhantomCameraTween = "get_trigger_pcam_tween";
+
+        public const string GetInterpolationMode = "get_interpolation_mode";
+        public const string SetInterpolationMode = "set_interpolation_mode";
+        
+        public const string SetHostLayersValue = "set_host_layers_value";
+    }
+}
+
+public class ActivePhantomCameraQueryResult(GodotObject godotObject)
+{
+    public bool Is2D => godotObject.IsClass("Node2D") || ((Node)godotObject).Name.ToString().Contains("PhantomCamera2D") 
+                                               || ((Node)godotObject).Name.ToString().Contains("PCam2D") 
+                                               || ((Node)godotObject).Name.ToString().Contains("2D");
+
+    public bool Is3D => godotObject.IsClass("Node3D") || ((Node)godotObject).Name.ToString().Contains("PhantomCamera3D") 
+                                               || ((Node)godotObject).Name.ToString().Contains("PCam3D") 
+                                               || ((Node)godotObject).Name.ToString().Contains("3D");
+
+    public PhantomCamera2D? AsPhantomCamera2D()
+    {
+        return Is2D ? new PhantomCamera2D(godotObject) : null;
+    }
+
+    public PhantomCamera3D? AsPhantomCamera3D()
+    {
+        return Is3D ? new PhantomCamera3D(godotObject) : null;
+    }
+}
