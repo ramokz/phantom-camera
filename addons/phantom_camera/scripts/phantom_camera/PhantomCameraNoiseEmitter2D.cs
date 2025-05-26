e73b6bb6c727f032bd7b02941bcebe37f22a42e6
Marcus Skov <Hello@MarcusSkov.com>
2025-05-26 17:32:44 +0100

    CSharp wrapper (#537)
    * C# Wrapper (#512)
    
    * WIP: Adding CSharp wrapper
    - Begin effort on PCam3D and some basic Godot extensions and PCam types
    
    * WIP: rough first pass at PhantomCamera with 2D and 3D classes
    
    * (#286) Finished the C# API
    
    * WIP #286: fixed bugs in c# wrapper and added tests
    - added Engine singleton registration to PhantomCameraManager
    - fixed incorrect LookAtMode signal (not available in 2d)
    - added c# project files
    - created basic test runner and added some tests (wip)
    
    * WIP #286: more tests and re-organized
    - fix several bugs from invalid getter/setters, properties, and types
    - added more tests
    - added LimitTarget query result type for working with TileMaps and CollisionShape2Ds
    - reorganized scripts since main script file had become quite large
    
    * wip: #286 C# Wrapper
    - added missing shared camera properties
    - added missing phantom camera 2d properties
    - added missing phantom camera 3d properties
    
    * WIP #268: catching up to release 0.7.3
    
    * WIP: Adding CSharp wrapper
    - Begin effort on PCam3D and some basic Godot extensions and PCam types
    
    * Fix addon not working correctly
    
    I hate merging manually
    
    * Implement PCamNoiseEmitter2D and PCamNoise2D
    
    * Add more tests, still unfinished
    
    * Address reviews
    
    * Finalize c# wrapper logic
    
    Added missing setters: if a variable had multiple setters, they were missing, so they were added.
    
    * Make all classes variant compatible
    
    * Revert "Make all classes variant compatible"
    
    This reverts commit 902b2f19c39f6244cbb5e68c1605fa758c2ff6be.
    
    * Partly address reviews
    
    - Removed dedicated dev scene for C# (and its script)
    - Removed junk files from the .csproj file
    - Fixed dev scene 3D having the wrong root node
    
    * Address remaining reviews
    
    - Removed ReSharper comment in PhantomCamera2D.cs
    - Added getters in PhantomCameraManager.cs
    - Removed the android and ios specific parts of the csproj
    
    * Remove left over doc comment
    
    * Revert dev_scene_3d.tscn to how it was before
    
    (idk why it was changed, it wasnt me, i just tried to fix it)
    
    * Add missing properties and methods from recent updates and address reviews
    
    - Added KeepAspect property and enum in Camera3DResource.cs
    - Removed PhantomCameraHostOwner property in  PhantomCamera.cs
    - Added TeleportPosition to PhantomCamera2D.cs
    - Replace Rotation properties with setters and getters in PhantomCamera3D.cs
    - Added missing methods in PhantomCameraNoiseEmitter2D.cs and PhantomCameraNoiseEmitter3D.cs
    - Made enum types match their GDScript counter-part in PhantomCameraTween.cs
    
    * Address reviews
    
    - Removed "Rotation" from GetThirdPersonQuaternion and its setter
    - Gave GetThirdPersonQuaternion and its setter the correct MethodName
    - Added LimitTarget property in PhantomCamera2D.cs
    - Removed constant floats from Camera3DResource.cs
    
    ---------
    
    Co-authored-by: Cody Bentley <cody.bentley.la@gmail.com>
    
    * Rotation fix (#534)
    
    * Reverted UIDs
    
    * Indentation formatting
    
    * Added new getter functions
    
    * Renamed function is_looking_at() to is_looking()
    
    * Additional formatted file
    
    * Fixed issue Array setters not working
    
    * Removed FollowTargetPosition and LookAtTargetPosition getters
    
    ---------
    
    Co-authored-by: GabCoolGuy <gabrielfreville@proton.me>
    Co-authored-by: Cody Bentley <cody.bentley.la@gmail.com>

---
+++ addons/phantom_camera/scripts/phantom_camera/PhantomCameraNoiseEmitter2D.cs
@@ -0,0 +1,83 @@
+﻿using Godot;
+
+namespace PhantomCamera.Noise;
+
+public class PhantomCameraNoiseEmitter2D(GodotObject node)
+{
+    public Node2D Node2D = (Node2D)node;
+
+    public PhantomCameraNoise2D Noise
+    {
+        get => new((Resource)Node2D.Call(MethodName.GetNoise));
+        set => Node2D.Call(MethodName.SetNoise, (GodotObject)value.Resource);
+    }
+
+    public bool Continuous
+    {
+        get => (bool)Node2D.Call(MethodName.GetContinuous);
+        set => Node2D.Call(MethodName.SetContinuous, value);
+    }
+
+    public float GrowthTime
+    {
+        get => (float)Node2D.Call(MethodName.GetGrowthTime);
+        set => Node2D.Call(MethodName.SetGrowthTime, value);
+    }
+
+    public float Duration
+    {
+        get => (float)Node2D.Call(MethodName.GetDuration);
+        set => Node2D.Call(MethodName.SetDuration, value);
+    }
+
+    public float DecayTime
+    {
+        get => (float)Node2D.Call(MethodName.GetDecayTime);
+        set => Node2D.Call(MethodName.SetDecayTime, value);
+    }
+
+    public int NoiseEmitterLayer
+    {
+        get => (int)Node2D.Call(MethodName.GetNoiseEmitterLayer);
+        set => Node2D.Call(MethodName.SetNoiseEmitterLayer, value);
+    }
+
+    public void SetNoiseEmitterLayerValue(int layer, bool value) =>
+        Node2D.Call(MethodName.SetNoiseEmitterLayerValue, layer, value);
+
+    public void Emit() => Node2D.Call(MethodName.Emit);
+
+    public bool IsEmitting() => (bool)Node2D.Call(MethodName.IsEmitting);
+
+    public void Stop() => Node2D.Call(MethodName.Stop);
+
+    public void Toggle() => Node2D.Call(MethodName.Toggle);
+
+    public static class MethodName
+    {
+        public const string GetNoise = "get_noise";
+        public const string SetNoise = "set_noise";
+
+        public const string GetContinuous = "get_continuous";
+        public const string SetContinuous = "set_continuous";
+
+        public const string GetGrowthTime = "get_growth_time";
+        public const string SetGrowthTime = "set_growth_time";
+
+        public const string GetDuration = "get_duration";
+        public const string SetDuration = "set_duration";
+
+        public const string GetDecayTime = "get_decay_time";
+        public const string SetDecayTime = "set_decay_time";
+
+        public const string GetNoiseEmitterLayer = "get_noise_emitter_layer";
+        public const string SetNoiseEmitterLayer = "set_noise_emitter_layer";
+
+        public const string SetNoiseEmitterLayerValue = "set_noise_emitter_layer_value";
+
+        public const string Emit = "emit";
+        public const string IsEmitting = "is_emitting";
+        public const string Stop = "stop";
+        public const string Toggle = "toggle";
+    }
+}
