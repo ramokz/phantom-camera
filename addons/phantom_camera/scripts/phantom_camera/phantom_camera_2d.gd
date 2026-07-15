0500e8918556f7b104cefcb5b73eaadfc0c66561
Joey <airsyad@proton.me>
2026-07-16 05:49:55 +0800

    chore: tidy up the logic

--- addons/phantom_camera/scripts/phantom_camera/phantom_camera_2d.gd
+++ addons/phantom_camera/scripts/phantom_camera/phantom_camera_2d.gd
@@ -1357,10 +1357,11 @@ func get_zoom() -> Vector2:
 
 ## Assigns new [member priority] value.
 func set_priority(value: int) -> void:
-	if priority == maxi(0, value): 
+	var new_priority: int = maxi(0, value)
+	if priority == new_priority: 
 		push_warning("Newly assigned priority value of " + name + " is identical to current.")
 		return
-	priority = maxi(0, value)
+	priority = new_priority
 	if not is_node_ready(): return
 	if not Engine.has_singleton(_constants.PCAM_MANAGER_NODE_NAME): return
 	Engine.get_singleton(_constants.PCAM_MANAGER_NODE_NAME).pcam_priority_changed.emit(self)
